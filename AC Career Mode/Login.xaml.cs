f392408b6411f057ecd0e35f30e54d99c0266472
Gaston <gastonbalparda@gmail.com>
2023-04-25 17:34:23 -0300

    Database working

---
+++ AC Career Mode/Login.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DemoLibrary;
+
+namespace AC_Career_Mode
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        List<Player> players = new List<Player>();
+        public Login()
+        {
+            InitializeComponent();
+            LoadPlayerList();
+        }
+
+        private void LoadPlayerList()
+        {
+            players = SqliteDataAccess.LoadPlayers();
+            WireUpPlayerList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Player p = new Player();
+            p.Name = tb_Name.Text;
+            p.Money = 10000;
+
+            SqliteDataAccess.SavePlayer(p);
+
+            tb_Name.Text = "";
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            LoadPlayerList();
+        }
+
+        private void WireUpPlayerList()
+        {
+            lb_Players.ItemsSource = null;
+            lb_Players.ItemsSource = players;
+            lb_Players.DisplayMemberPath = "Name";
+
+        }
+    }
+}
---
+++ DemoLibrary/SqliteDataAccess.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using Dapper;
+
+namespace DemoLibrary
+{
+    public class SqliteDataAccess
+    {
+        public static List<Player> LoadPlayers()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<Player>("select * from Player", new DynamicParameters());
+                return output.ToList();
+            }
+
+        }
+
+        public static void SavePlayer(Player pName)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("insert into Player (Name, Money) values (@Name, @Money)", pName);
+            }
+        }
+
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
