858bebfb48b888fe716b6c1a573ad598f2b5e7e2
Gaston <gastonbalparda@gmail.com>
2023-04-30 01:14:10 -0300

    Updated db for future changes

--- AC Career Mode/Classes/GlobalVars.cs
+++ AC Career Mode/Classes/GlobalVars.cs
@@ -5,9 +5,9 @@
         internal static string TRACKS_PATH = @"C:\Program Files (x86)\Steam\steamapps\common\assettocorsa\content\tracks";
         internal static string CARS_PATH = @"C:\Program Files (x86)\Steam\steamapps\common\assettocorsa\content\cars";
 
-        internal static string TracksBin = @".\bins\ACTracks.bin";
-        internal static string RacesBin = @".\bins\ACRaces.bin";
-        internal static string CarsBin = @".\bins\ACCars.bin";
+        internal static string TracksBin = @".\bins\ACTracksData.bin";
+        internal static string RacesBin = @".\bins\ACRacesData.bin";
+        internal static string CarsBin = @".\bins\ACCarsData.bin";
 
         internal static string DailyCarBin = @".\bins\Market.bin";
     }
