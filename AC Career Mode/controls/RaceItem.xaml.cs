48d214b111ca978ed63f5b28b21dc62f51e988da
Gaston <gastonbalparda@gmail.com>
2023-05-17 11:03:55 -0300

    Working race item

--- AC Career Mode/controls/RaceItem.xaml.cs
+++ AC Career Mode/controls/RaceItem.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DBLink.Classes;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +17,39 @@ using System.Windows.Shapes;
 
 namespace AC_Career_Mode.controls
 {
-    /// <summary>
-    /// Interaction logic for RaceItem.xaml
-    /// </summary>
     public partial class RaceItem : UserControl
     {
+
+        public Race race
+        {
+            get { return (Race)GetValue(RaceProperty); }
+            set { SetValue(RaceProperty, value); }
+        }
+
+        public static readonly DependencyProperty RaceProperty = DependencyProperty.Register(
+                                                                                            "race",
+                                                                                            typeof(Race),
+                                                                                            typeof(RaceItem),
+                                                                                            new PropertyMetadata(null, OnRacePropertyChanged));
+
         public RaceItem()
         {
             InitializeComponent();
+        }
+
+
+        private static void OnRacePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            RaceItem raceItem = (RaceItem)d;
+            Race newRace = (Race)e.NewValue;
+
+            if (newRace != null)
+            {
+                raceItem.lb_Car.Content = newRace.Car.Name;
+                raceItem.lb_Track.Content = newRace.Track.Name;
+                raceItem.lb_Prize.Content = newRace.Prize;
+                raceItem.lb_Laps.Content = newRace.Laps + " laps";
+            }
         }
     }
 }
